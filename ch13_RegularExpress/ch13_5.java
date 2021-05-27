16f5339ddf855c37198026e3b781709bf6eb967a
ust <peustivate@gmail.com>
2021-05-28 05:06:27 +0800

    reg rest

--- ch13_RegularExpress/ch13_5.java
+++ ch13_RegularExpress/ch13_5.java
@@ -11,6 +11,12 @@ public class ch13_5 {
 		System.out.println("0952-909-789是手機號碼" +s2.matches(pattern));
 		System.out.println("1111-111111是手機號碼" +s3.matches(pattern));
 
+
+		String pa = "\\d{4}(-\\d{3}){2}";
+		String pa2 = "\\d{4}-\\d{6}";
+		System.out.println("I Love Java是手機號碼" +s1.matches(pa));
+		System.out.println("0952-909-789是手機號碼" +s2.matches(pa));
+		System.out.println("1111-111111是手機號碼" +s3.matches(pa2));
 	}
 
 }
